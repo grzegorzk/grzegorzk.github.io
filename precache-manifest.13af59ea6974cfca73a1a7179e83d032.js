42cc15b15ecc27cdac245dac40d74b3ca874c57f
grzegorzk <grzegorz.klimaszewski@yahoo.com>
2019-10-06 10:15:54 +0200

    Replacing react build - step 2 (Sun  6 Oct 10:15:54 CEST 2019)

---
+++ precache-manifest.13af59ea6974cfca73a1a7179e83d032.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5c12b82a23d584ff5e67c6c9fa215b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05c0c270bef078839d29",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "092e201ffc96f42d6bd1",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "05c0c270bef078839d29",
+    "url": "/static/js/2.07a2f632.chunk.js"
+  },
+  {
+    "revision": "092e201ffc96f42d6bd1",
+    "url": "/static/js/main.aa35a456.chunk.js"
+  },
+  {
+    "revision": "268f4ae3f3d27f176f90",
+    "url": "/static/js/runtime~main.c392ca3d.js"
+  }
+]);
