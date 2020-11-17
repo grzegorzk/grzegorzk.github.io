9d4e816cb49dc6508a4152b276214c8cda933211
Grzegorz Klimaszewski <2612193+grzegorzk@users.noreply.github.com>
2020-11-17 12:35:51 +0100

    Replacing react build - step 2 (wto, 17 lis 2020, 12:35:51 CET)

---
+++ precache-manifest.c3995b3d3b6c85f943f385957b83e559.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "510271c1ff0b5348683a2557b90e9d1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07d4a81525a74be0b9aa",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "2f9eb002f24f0c7e83fd",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "07d4a81525a74be0b9aa",
+    "url": "/static/js/2.75ab4b7b.chunk.js"
+  },
+  {
+    "revision": "2f9eb002f24f0c7e83fd",
+    "url": "/static/js/main.a4c324b3.chunk.js"
+  },
+  {
+    "revision": "268f4ae3f3d27f176f90",
+    "url": "/static/js/runtime~main.c392ca3d.js"
+  }
+]);
